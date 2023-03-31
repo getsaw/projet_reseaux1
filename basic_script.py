883003a7beed98c05be1ae95c3f49ab9cd983054
getsaw <75913539+getsaw@users.noreply.github.com>
2023-03-31 14:51:50 +0100

    update basic script

--- basic_script.py
+++ basic_script.py
@@ -1,9 +1,24 @@
 import pyshark
-def analyse():
-    capture = pyshark.FileCapture("Traces/trace_sans_actions.pcapng")
+
+autoritative = []
+
+def analyse(filename, list):
+    capture = pyshark.FileCapture(filename, display_filter="dns")
     for packet in capture:
-        if "DNS" in packet and not packet.dns.flags_response.int_value:
-            print(packet.dns.qry_name)
+        if not packet.dns.flags_response.int_value:
+            if hash(packet.dns.qry_name) not in list:
+                list.add(hash(packet.dns.qry_name))
+                print(packet.dns.qry_name)
+        if packet.dns.count_auth_rr.int_value > 0:
+            autoritative.append(packet.dns.soa_rname)
+
+
+def run():
+    name_servers = set()
+    analyse("Traces/trace_sans_actions.pcapng", name_servers)
+    analyse("Traces/ouverture_messenger.pcapng", name_servers)
+    print(len(name_servers))
+    print(autoritative)
 
 if __name__ == '__main__':
-    analyse()
+    run()
